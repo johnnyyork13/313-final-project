f62c67fcb6ff2b6dc8527e1b2873719992d36d2b
johnnyyork13 <johnnyyork13@hotmail.com>
2023-12-10 10:26:21 -0500

    add test post fetch

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ import './App.css'
 function App() {
   const [count, setCount] = React.useState(0)
 
+  const url = 'https://us-central1-database-class-backend.cloudfunctions.net/api/test'
+  // const url = 'http://localhost:3000/test';
+
   React.useEffect(() => {
     async function testFetch() {
-      const url = 'https://us-central1-database-class-backend.cloudfunctions.net/api/test'
-      // const url = 'http://localhost:3000/test';
       try {
-        await fetch(url, {mode: "cors"})
+        await fetch(url, {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        })
         .then((res) => res.json())
         .then((data) => console.log("data", data));
       } catch(err) {
